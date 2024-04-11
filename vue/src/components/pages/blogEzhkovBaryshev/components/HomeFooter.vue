c203f5093f79f9148a5297c8e0e58250a0f62e13
baryshev_egor <lord3000ter@icloud.com>
2024-04-11 16:38:46 +0300

    second fixes on HomeFooter.vue

--- vue/src/components/pages/blogEzhkovBaryshev/components/HomeFooter.vue
+++ vue/src/components/pages/blogEzhkovBaryshev/components/HomeFooter.vue
@@ -23,6 +23,22 @@
           </li>
         </ul>
       </section>
+      <section class="footer__right-buttons">
+        <ul class="footer-list">
+          <li class="footer-list__item footer-list__first">
+            <a href="#Check_in" class="footer-list__link">Social</a>
+          </li>
+          <li class="footer-list__item footer-list__second">
+            <a href="#Check_in" class="footer-list__link">Business and groups</a>
+          </li>
+          <li class="footer-list__item footer-list__third">
+            <a href="#Log In" class="footer-list__link">Language</a>
+          </li>
+            <li class="footer-list__item header-list__first">
+              <button class="footer-list__first-button"/>
+            </li>
+        </ul>
+      </section>
     </div>
 
     </div>
@@ -49,7 +65,7 @@
 .footer-logo {
   display: flex;
   margin-left: 165px;
-  margin-bottom: 114px;
+  margin-bottom: 90px;
 }
 .footer-logo__img {
   margin-top: 30px;
@@ -65,6 +81,10 @@
   background: transparent;
   margin-left: 400px;
 }
+.footer__right-buttons{
+  background: transparent;
+  margin-left: 240px;
+}
 .footer-list {
   list-style-type: none;
 }
@@ -74,6 +94,36 @@
 .footer-list__link{
   color: white;
   font-size: 22px;
+}
+.footer-list__first-button {
+  height: 45px;
+  width: 175px;
+  border-radius: 20px;
+  border-color: white;
+  position: relative;
+  border-style: solid;
+  margin-top: -5px;
+  background-color: transparent;
+  color: white;
+
+  &::before {
+    font-size: 18px;
+    content: 'English';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    right: 50px;
+    top: 12px;
+  }
+  &::after{
+    font-size: 20px;
+    content:'ᐁ';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 60px;
+    top: 8px;
+  }
 }
 </style>
 <script setup lang="ts">
