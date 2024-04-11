b6b012a61bcc89c4721201aabd0ba5cdbeff1b44
baryshev_egor <lord3000ter@icloud.com>
2024-04-11 16:17:58 +0300

    first fixes on HomeFooter.vue

--- vue/src/components/pages/blogEzhkovBaryshev/components/HomeFooter.vue
+++ vue/src/components/pages/blogEzhkovBaryshev/components/HomeFooter.vue
@@ -2,24 +2,24 @@
   <div class="footer">
     <div class="footer-flex">
     <div class="footer-logo">
-      <img class="footer-logo--img" src="../images/Logo.svg" alt="Логотип сайта">
-      <h2 class = "footer-logo-text" >RoamStay</h2>
+      <img class="footer-logo__img" src="../images/Logo.svg" alt="Логотип сайта">
+      <h2 class = "footer-logo__text" >RoamStay</h2>
       <section class="footer__middle-buttons">
-        <ul class="header-list">
-          <li class="header-list-item header-list__first">
-            <a href="#Check_in" class="header-list__link">About us</a>
+        <ul class="footer-list">
+          <li class="footer-list__item footer-list__first">
+            <a href="#Check_in" class="footer-list__link">About us</a>
           </li>
-          <li class="header-list-item header-list__second">
-            <a href="#Check_in" class="header-list__link">Business and groups</a>
+          <li class="footer-list__item footer-list__second">
+            <a href="#Check_in" class="footer-list__link">Business and groups</a>
           </li>
-          <li class="header-list-item header-list__third">
-            <a href="#Log In" class="header-list__link">Support</a>
+          <li class="footer-list__item footer-list__third">
+            <a href="#Log In" class="footer-list__link">Support</a>
           </li>
-          <li class="header-list-item header-list__third">
-            <a href="#Log In" class="header-list__link">Responsibility</a>
+          <li class="footer-list__item footer-list__third">
+            <a href="#Log In" class="footer-list__link">Responsibility</a>
           </li>
-          <li class="header-list-item header-list__third">
-            <a href="#Log In" class="header-list__link">Blog</a>
+          <li class="footer-list__item footer-list__third">
+            <a href="#Log In" class="footer-list__link">Blog</a>
           </li>
         </ul>
       </section>
@@ -51,12 +51,12 @@
   margin-left: 165px;
   margin-bottom: 114px;
 }
-.footer-logo--img {
+.footer-logo__img {
   margin-top: 30px;
   height: 40px;
   padding-right: 25px;
 }
-.footer-logo-text {
+.footer-logo__text {
   margin-top: 30px;
   color: white;
   font-size: 33px;
@@ -65,13 +65,13 @@
   background: transparent;
   margin-left: 400px;
 }
-.header-list {
+.footer-list {
   list-style-type: none;
 }
-.header-list-item:nth-child(n+1) {
+.footer-list__item:nth-child(n+1) {
   margin-top: 35px;
 }
-.header-list__link{
+.footer-list__link{
   color: white;
   font-size: 22px;
 }
